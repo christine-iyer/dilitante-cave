e98a466f56a0030b5a942435b0dd439cfed58bbb
chrisIyer <hellohotbox420@gmail.com>
2025-04-28 16:46:43 -0400

    batabase not ther yet.

--- models/students.py
+++ models/students.py
@@ -1,6 +1,15 @@
-from pydantic import BaseModel
-from typing import List
+# from pydantic import BaseModel
+# from typing import List
 
-class Student(BaseModel):
-    full_name: str
-    reason: str
+# class Student(BaseModel):
+#     full_name: str
+#     reason: str
+from sqlalchemy import Column, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Student(Base):
+    __tablename__ = "students"
+    full_name = Column(String, primary_key=True, index=True)
+    reason = Column(String)
