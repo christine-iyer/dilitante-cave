b314bae1b29a8e3fac8f9debc09cfdda33ce3958
chrisIyer <hellohotbox420@gmail.com>
2025-05-04 13:40:18 -0400

    setting up cors

--- main.py
+++ main.py
@@ -4,6 +4,15 @@ from routes.instructors import router as instructors_router
 from routes.workshops import router as workshops_router
 app = FastAPI()
 
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # React frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 # Include the students router
 app.include_router(students_router)
 # Include the instructors router
