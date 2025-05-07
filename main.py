514803dd0bbf5386d866dba683f726f7ea13e2eb
chrisIyer <hellohotbox420@gmail.com>
2025-05-07 16:04:10 -0400

    all I had to do was chan ge allow origins

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000\"],  # React frontend URL
+    allow_origins=["http://localhost:5173"],  # React frontend URL
     allow_credentials=True,
     allow_methods=["*"],  # Allow all HTTP methods (GET, POST, PUT, DELETE, etc.)
     allow_headers=["*"],  # Allow all headers
