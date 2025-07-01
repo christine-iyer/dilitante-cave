7102e0aee82733fb07dd0a6d759f082a1f2f322d
chrisIyer <hellohotbox420@gmail.com>
2025-07-01 11:56:56 -0400

    testing register is givinig 404 because auth router is not being called. Next step is to follow steps 1. Missing Route Incluision.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from auth import router as auth_router
 from routes.students import router as students_router
 from routes.instructors import router as instructors_router
 from routes.workshops import router as workshops_router
@@ -16,6 +17,7 @@ app.add_middleware(
 )
 
 # Include routers
+app.include_router(auth_router)
 app.include_router(students_router)
 app.include_router(instructors_router)
 app.include_router(workshops_router)    
